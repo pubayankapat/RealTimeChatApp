3f6755d03025ce53715ad8bb962e7b5d2f135391
pubayankapat <pubayankapat@gmail.com>
2025-11-23 16:01:14 +0530

    bug fixes

--- chat-server/index.js
+++ chat-server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT;
 app.get('/',(req,res)=>{
     res.send("Server working");
 })
-server.listen(PORT, ()=>{
+server.listen(PORT, "0.0.0.0", ()=>{
     console.log(`Server running`);
 })
 
