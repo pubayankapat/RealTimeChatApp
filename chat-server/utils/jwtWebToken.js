99eef65932518bf76dfb4fee46d6878ccf2a0769
pubayankapat <pubayankapat@gmail.com>
2025-11-25 17:42:21 +0530

    Bug fixes

--- chat-server/utils/jwtWebToken.js
+++ chat-server/utils/jwtWebToken.js
@@ -1,17 +1,18 @@
 import jwt from "jsonwebtoken";
 
-const jwtToken = (userId, res)=>{
-    const token = jwt.sign({userId}, process.env.SECRET_KEY,{
-        expiresIn:'30d'
-    })
-    res.cookie('jwt',token),{
-        maxAge: 30 * 24 * 60 * 60 * 1000,
+const jwtToken = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.SECRET_KEY, {
+        expiresIn: "30d",
+    });
+
+    res.cookie("token", token, {
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         httpOnly: true,
-        sameSite:'strict',
-        secure:process.env.SECURE !== 'development'
-    }
+        sameSite: "strict",
+        secure: process.env.SECURE !== "development", 
+    });
 
-}
+    return token;
+};
 
-
-export default jwtToken
+export default jwtToken;
