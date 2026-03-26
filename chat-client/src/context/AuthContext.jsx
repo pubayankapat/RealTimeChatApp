dd6ce8025278a186a5d4f81d42a83aa6603e0e6f
pubayankapat <pubayankapat@gmail.com>
2026-03-26 15:45:20 +0530

    Changes

--- chat-client/src/context/AuthContext.jsx
+++ chat-client/src/context/AuthContext.jsx
@@ -4,11 +4,24 @@ import { createContext, useContext, useState } from "react";
 export const AuthContext = createContext();
 
 export const useAuth = () => {
-    return useContext(AuthContext);
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthContextProvider");
+    }
+    return context;
 };
 
 export const AuthContextProvider = ({ children }) => {
-    const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem('chatrix',)) || null);
+    const [authUser, setAuthUser] = useState(() => {
+        try {
+            const storedUser = localStorage.getItem("chatrix");
+            return storedUser ? JSON.parse(storedUser) : null;
+        } catch (error) {
+            console.error("Error parsing authUser from localStorage:", error);
+            return null;
+        }
+    });
+
     return (
         <AuthContext.Provider value={{ authUser, setAuthUser }}>
             {children}
